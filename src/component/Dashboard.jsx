9f1bd12a3acef523d11b601a6f7a6ac844bc2e10
ParthGupta84616 <parthguptaji20@gmail.com>
2024-08-28 17:53:42 +0530

    Added Select Method , Input , Send button

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React from "react";
+import { ComboboxDemo } from "./ComboBox";
 // import { Button } from '../components/ui/button'
+/* Add this CSS to your stylesheet */
+import "../animation.css";
+import { Button } from "../components/ui/button";
+
 
 function Dashboard() {
   return (
-    <div>
-        {/* <Button>Click me</Button> */}
+    <div className="relative">
+      <div className="absolute "></div>
+      <div className="relative flex justify-center items-center gap-2">
+        <ComboboxDemo />
+        <main class="">
+          <label className="flex flex-col-reverse relative focus group">
+            <input
+              type="URL"
+              name="URL"
+              required
+              placeholder="Enter URL or paste text"
+              className="border-2 h-14 w-[850px] text-xl font-semibold left-4 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent placeholder-gray-500 pl-2"
+            />
+          </label>
+        </main>
+        <Button size="send" varient="default" >
+          Send
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Logo from "../assests/TestPilot.jpg";
+import { Github, Linkedin } from 'lucide-react';
 
 function Navbar() {
   return (
     <div className="relative">
       <div className="absolute inset-0  backdrop-blur-md "></div>
-      <div className="relative text-gray-800 hover:text-gray-900">
+      <div className="relative text-gray-200 hover:text-gray-300">
         <div className="mx-auto max-w-screen-xl px-6 lg:px-8">
           <div className="relative flex h-24 space-x-10 w-full font-bold ">
             <div className="flex justify-start">
@@ -16,10 +17,14 @@ function Navbar() {
                 </h1>
               </a>
             </div>
-            <div className="flex-shrink-0 flex px-2 py-3 items-center space-x-8 flex-1 justify-end justify-self-end">
-              <a className="text-md " href="https://www.linkedin.com/in/parth-guptaji/">Linkedln</a>
-              <a className="inline-flex items-center justify-center px-3 py-2 border border-transparent text-md  rounded-md shadow-sm bg-slate-400 hover:bg-slate-500"
-                href="https://github.com/ParthGupta84616">Github</a>
+            <div className="flex-shrink-0 flex px-2 py-3 items-center space-x-8 flex-1 justify-end justify-self-end ">
+              <a className="inline-flex items-center justify-center px-3 py-2 border border-transparent text-md  rounded-md shadow-sm hover:bg-slate-500 text-md gap-2" href="https://www.linkedin.com/in/parth-guptaji/"> 
+              <Linkedin size={20} /> | 
+              Linkedln</a>
+              <a className="inline-flex items-center justify-center gap-2 px-3 py-2 border border-transparent text-md  rounded-md shadow-sm bg-slate-600 hover:bg-slate-500"
+                href="https://github.com/ParthGupta84616">
+                  <Github size={20}  /> | 
+                  Github</a>
             </div>
           </div>
         </div>
